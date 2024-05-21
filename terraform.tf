bb901786120a1fc44345eaa75cffad2e309ddd0d
Shel Soloa <shel@shelsoloa.com>
2024-05-21 00:54:30 -0400

    remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -3,13 +3,15 @@
 
 terraform {
 
+/*
   cloud {
-    organization = "organization-name"
+    organization = "PromoExchange"
 
     workspaces {
       name = "learn-terraform"
     }
   }
+*/
 
   required_providers {
     aws = {
